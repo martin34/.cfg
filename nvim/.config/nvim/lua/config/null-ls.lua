dfc41d3df3212b9cf2e530647018fa24a441fce3
martin <martingerwallner@gmx.de>
2023-05-23 21:44:59 +0200

    Add line length to black formater

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -30,7 +30,9 @@ function M.setup()
     local null_ls = require("null-ls")
     null_ls.setup({
         sources = {
-            null_ls.builtins.formatting.black,
+            null_ls.builtins.formatting.black.with({
+                extra_args = { "-l", "120" },
+            }),
             null_ls.builtins.formatting.isort,
             null_ls.builtins.formatting.autoflake,
             null_ls.builtins.formatting.buildifier,
