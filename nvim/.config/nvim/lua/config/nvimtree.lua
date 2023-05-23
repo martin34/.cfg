ef3a63fe239a3c751f29d1168e0ef90da4293cc9
martin <martingerwallner@gmx.de>
2023-05-23 22:01:47 +0200

    Fix nvim tree toggle

--- nvim/.config/nvim/lua/config/nvimtree.lua
+++ nvim/.config/nvim/lua/config/nvimtree.lua
@@ -2,18 +2,14 @@ local M = {}
 local nvim_tree = require'nvim-tree'
 local api = require'nvim-tree.api'
 local nvim_tree_lib = require'nvim-tree.lib'
-local change_dir = require'nvim-tree.actions.root.change-dir'
 local telescope = require'telescope.builtin'
-local util = require 'lspconfig.util'
-local core = require "nvim-tree.core"
 
 function M.nvim_tree_find_file()
   api.tree.open({find_file=true})
 end
 
 function M.nvim_tree_toggle_project()
-    change_dir.fn(vim.g.project_path)
-    nvim_tree.toggle()
+    api.tree.toggle()
 end
 
 local function grep_at_current_tree_node()
